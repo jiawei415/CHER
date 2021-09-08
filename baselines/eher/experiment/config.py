acb791987728610c62bb00874ef2d812b1e48fa8
ztjiaweixu <ztjiaweixu@tencent.com>
2021-09-08 19:07:01 +0800

    copy herebp as eher

---
+++ baselines/eher/experiment/config.py
@@ -0,0 +1,251 @@
+from copy import deepcopy
+import numpy as np
+import json
+import os
+import gym
+
+from baselines import logger
+from baselines.envs.multi_world_wrapper import PointGoalWrapper, SawyerGoalWrapper, ReacherGoalWrapper
+from baselines.eher.ddpg import DDPG
+from baselines.eher.her import make_sample_her_transitions, \
+                              make_sample_her_transitions_energy, \
+                              make_sample_her_transitions_prioritized_replay
+
+
+DEFAULT_ENV_PARAMS = {
+    'Point2D':
+        {'n_cycles': 1, 'batch_size': 64, 'n_batches': 5,},
+    'SawyerReach':
+        {'n_cycles': 5, 'batch_size': 64, 'n_batches': 5,},
+    'FetchReach':
+        {'n_cycles': 5, 'batch_size': 64, 'n_batches': 5,},
+    'Reacher-v2':
+        {'n_cycles': 15, 'batch_size': 64, 'n_batches': 5,},
+    'PointMass':
+        {'n_cycles': 50, 'batch_size': 256, 'n_batches': 40,},
+    'Sawyer':
+        {'n_cycles': 50, 'batch_size': 256, 'n_batches': 40,},
+    'Fetch':
+        {'n_cycles': 50, 'batch_size': 256, 'n_batches': 40,},
+    'Hand':
+        {'n_cycles': 50, 'batch_size': 256, 'n_batches': 40,},
+}
+
+
+DEFAULT_PARAMS = {
+    # env
+    'max_u': 1.,  # max absolute value of actions on different coordinates
+    'max_episode_steps': 50,
+    # ddpg
+    'layers': 3,  # number of layers in the critic/actor networks
+    'hidden': 256,  # number of neurons in each hidden layers
+    'network_class': 'baselines.her.actor_critic:ActorCritic',
+    'Q_lr': 0.001,  # critic learning rate
+    'pi_lr': 0.001,  # actor learning rate
+    'buffer_size': int(1E6),  # int(1E6) int(1E6) bug for experience replay 
+    'polyak': 0.9,  # polyak averaging coefficient
+    'action_l2': 1.0,  # quadratic penalty on actions (before rescaling by max_u)
+    'clip_obs': 200.,
+    'scope': 'ddpg',  # can be tweaked for testing
+    'relative_goals': False,
+    # training
+    'n_cycles': 50,  # per epoch
+    'rollout_batch_size': 1,  # per mpi thread
+    'n_batches': 40,  # training batches per cycle
+    'batch_size': 64,  # per mpi thread, measured in transitions and reduced to even multiple of chunk_length.
+    'n_test_rollouts': 100,  # number of test rollouts per epoch, each consists of rollout_batch_size rollouts
+    'test_with_polyak': False,  # run test episodes with the target network
+    # exploration
+    'random_eps': 0.3,  # percentage of time a random action is taken
+    'noise_eps': 0.2,  # std of gaussian noise added to not-completely-random actions as a percentage of max_u
+    # HER
+    'replay_strategy': 'future',  # supported modes: future, none
+    'replay_k': 4,  # number of additional goals used for replay, only used if off_policy_data=future
+    # normalization
+    'norm_eps': 1e-4,  # epsilon used for observation normalization
+    'norm_clip': 5,  # normalized observations are cropped to this values
+
+    # prioritized_replay (tderror)
+    'alpha': 0.6, # 0.6
+    'beta0': 0.4, # 0.4
+    'beta_iters': None, # None
+    'eps': 1e-6,
+
+    # energy-based prioritization
+    'w_potential': 1.0,
+    'w_linear': 1.0,
+    'w_rotational': 1.0,
+
+    # random init episode
+    'random_init': 20,
+}
+
+
+CACHED_ENVS = {}
+
+
+def cached_make_env(make_env):
+    """
+    Only creates a new environment from the provided function if one has not yet already been
+    created. This is useful here because we need to infer certain properties of the env, e.g.
+    its observation and action spaces, without any intend of actually using it.
+    """
+    if make_env not in CACHED_ENVS:
+        env = make_env()
+        CACHED_ENVS[make_env] = env
+    return CACHED_ENVS[make_env]
+
+
+def prepare_params(kwargs):
+    # DDPG params
+    ddpg_params = dict()
+    # default max episode steps
+    default_max_episode_steps = 100
+    env_name = kwargs['env_name']
+
+    def make_env(subrank=None):
+        try:
+            env = gym.make(env_name, rewrad_type='sparse')
+        except:
+            logger.log('Can not make sparse reward environment')
+            env = gym.make(env_name)
+        # add wrapper for multiworld environment
+        if env_name.startswith('Fetch'):
+            env._max_episode_steps = 100
+        elif env_name.startswith('Hand'):
+            env._max_episode_steps = 100
+        elif env_name.startswith('Point'):
+            env = PointGoalWrapper(env)
+            env.env._max_episode_steps = 100
+        elif env_name.startswith('Sawyer'):
+            env = SawyerGoalWrapper(env)
+        elif env_name.startswith('Reacher'):
+            env = ReacherGoalWrapper(env)
+
+        if (subrank is not None and logger.get_dir() is not None):
+            try:
+                from mpi4py import MPI
+                mpi_rank = MPI.COMM_WORLD.Get_rank()
+            except ImportError:
+                MPI = None
+                mpi_rank = 0
+                logger.warn('Running with a single MPI process. This should work, but the results may differ from the ones publshed in Plappert et al.')
+
+            if hasattr(env, '_max_episode_steps'):
+                max_episode_steps = env._max_episode_steps
+            else:
+                max_episode_steps = default_max_episode_steps # otherwise use defaulit max episode steps
+            env =  Monitor(env,
+                           os.path.join(logger.get_dir(), str(mpi_rank) + '.' + str(subrank)),
+                           allow_early_resets=True)
+            # hack to re-expose _max_episode_steps (ideally should replace reliance on it downstream)
+            env = gym.wrappers.TimeLimit(env, max_episode_steps=max_episode_steps)
+        return env
+
+    kwargs['make_env'] = make_env
+    kwargs['T'] = kwargs['max_episode_steps']
+    kwargs['max_u'] = np.array(kwargs['max_u']) if type(kwargs['max_u']) == list else kwargs['max_u']
+    kwargs['gamma'] = 1. - 1. / kwargs['T']
+    if 'lr' in kwargs:
+        kwargs['pi_lr'] = kwargs['lr']
+        kwargs['Q_lr'] = kwargs['lr']
+        del kwargs['lr']
+    for name in ['buffer_size', 'hidden', 'layers',
+                 'network_class',
+                 'polyak', 
+                 'batch_size', 'Q_lr', 'pi_lr',
+                 'norm_eps', 'norm_clip', 'max_u',
+                 'action_l2', 'clip_obs', 'scope', 'relative_goals',
+                 'alpha', 'beta0', 'beta_iters', 'eps']:
+        ddpg_params[name] = kwargs[name]
+        kwargs['_' + name] = kwargs[name]
+        del kwargs[name]
+    kwargs['ddpg_params'] = ddpg_params
+
+    return kwargs
+
+
+def log_params(params, logger=logger):
+    for key in sorted(params.keys()):
+        logger.info('{}: {}'.format(key, params[key]))
+
+
+def configure_her(params):
+    env = cached_make_env(params['make_env'])
+    env.reset()
+    def reward_fun(ag_2, g, info):  # vectorized
+        return env.compute_reward(achieved_goal=ag_2, desired_goal=g, info=info)
+
+    # Prepare configuration for HER.
+    her_params = {
+        'reward_fun': reward_fun,
+    }
+    for name in ['replay_strategy', 'replay_k']:
+        her_params[name] = params[name]
+        params['_' + name] = her_params[name]
+        del params[name]
+
+    if params['prioritization'] == 'energy':
+        sample_her_transitions = make_sample_her_transitions_energy(**her_params)
+    elif params['prioritization'] == 'tderror':
+        sample_her_transitions = make_sample_her_transitions_prioritized_replay(**her_params)
+    else:
+        sample_her_transitions = make_sample_her_transitions(**her_params)
+
+    return sample_her_transitions
+
+
+def simple_goal_subtract(a, b):
+    assert a.shape == b.shape
+    return a - b
+
+
+def configure_ddpg(dims, params, reuse=False, use_mpi=True, clip_return=True):
+    sample_her_transitions = configure_her(params)
+    # Extract relevant parameters.
+    gamma = params['gamma']
+    rollout_batch_size = params['rollout_batch_size']
+    ddpg_params = params['ddpg_params']
+    temperature = params['temperature']
+    prioritization = params['prioritization']
+    env_name = params['env_name']
+    max_timesteps = params['max_timesteps']
+    rank_method = params['rank_method']
+
+    input_dims = dims.copy()
+
+    # DDPG agent
+    env = cached_make_env(params['make_env'])
+    env.reset()
+    ddpg_params.update({'input_dims': input_dims,  # agent takes an input observations
+                        'T': params['T'],
+                        'clip_pos_returns': True,  # clip positive returns
+                        'clip_return': (1. / (1. - gamma)) if clip_return else np.inf,  # max abs of return
+                        'rollout_batch_size': rollout_batch_size,
+                        'subtract_goals': simple_goal_subtract,
+                        'sample_transitions': sample_her_transitions,
+                        'gamma': gamma,
+                        'temperature': temperature,
+                        'prioritization': prioritization,
+                        'env_name': env_name,
+                        'max_timesteps': max_timesteps,
+                        'rank_method': rank_method,
+                        })
+    ddpg_params['info'] = {
+        'env_name': params['env_name'],
+    }
+    policy = DDPG(reuse=reuse, **ddpg_params, use_mpi=use_mpi)
+    return policy
+
+
+def configure_dims(params):
+    env = cached_make_env(params['make_env'])
+    env.reset()
+    obs, _, _, info = env.step(env.action_space.sample())
+
+    dims = {
+        'o': obs['observation'].shape[0],
+        'u': env.action_space.shape[0],
+        'g': obs['desired_goal'].shape[0],
+    }
+    return dims
---
+++ baselines/eher/experiment/train.py
@@ -0,0 +1,276 @@
+import os
+import sys
+import copy
+import time
+import json
+import click
+import os.path as osp
+import numpy as np
+from mpi4py import MPI
+
+from baselines import logger
+from baselines.common import set_global_seeds
+from baselines.common.mpi_moments import mpi_moments
+from baselines.common.env_util import build_env, get_game_envs
+from baselines.her.rollout import RolloutWorker
+from baselines.her.util import mpi_fork
+import baselines.eher.experiment.config as config
+
+_game_envs = get_game_envs(print_out=False)
+
+
+def mpi_average(value):
+    if value == []:
+        value = [0.]
+    if not isinstance(value, list):
+        value = [value]
+    return mpi_moments(np.array(value))[0]
+
+
+def train(policy, rollout_worker, evaluator, n_epochs, n_test_rollouts, n_cycles, n_batches, 
+          policy_save_interval, save_policies, num_cpu, dump_buffer, w_potential, w_linear,
+          w_rotational, rank_method, clip_energy, random_init, **kwargs):
+    rank = MPI.COMM_WORLD.Get_rank()
+
+    latest_policy_path = os.path.join(logger.get_dir(), 'policy_latest.pkl')
+    best_policy_path = os.path.join(logger.get_dir(), 'policy_best.pkl')
+    periodic_policy_path = os.path.join(logger.get_dir(), 'policy_{}.pkl')
+
+    # random_init for o/g/rnd stat and model training
+    if random_init:
+        logger.info('Random initializing ...')
+        rollout_worker.clear_history()
+        # rollout_worker.render = True
+        for epi in range(int(random_init) // rollout_worker.rollout_batch_size): 
+            episode = rollout_worker.generate_rollouts(random_ac=True)
+            policy.store_episode(episode, dump_buffer, w_potential, w_linear, w_rotational, rank_method, clip_energy)
+
+    logger.info("Training...")
+    best_success_rate = -1
+    t = 1
+    for epoch in range(n_epochs):
+        # train
+        time_start = time.time()
+        rollout_worker.clear_history()
+        for cycle in range(n_cycles):
+            episode = rollout_worker.generate_rollouts()
+            policy.store_episode(episode, dump_buffer, w_potential, w_linear, w_rotational, rank_method, clip_energy)
+            for batch in range(n_batches):
+                t = ((epoch*n_cycles*n_batches)+(cycle*n_batches)+batch)*num_cpu
+                policy.train(t, dump_buffer)
+
+            policy.update_target_net()
+
+        # test
+        evaluator.clear_history()
+        for _ in range(n_test_rollouts):
+            evaluator.generate_rollouts()
+
+        # record logs
+        time_end = time.time()
+        total_time = time_end - time_start
+        logger.record_tabular('epoch/num', epoch)
+        logger.record_tabular('epoch/time(min)', total_time/60)
+        for key, val in evaluator.logs('test'):
+            logger.record_tabular(key, mpi_average(val))
+        for key, val in rollout_worker.logs('train'):
+            logger.record_tabular(key, mpi_average(val))
+        for key, val in policy.logs():
+            logger.record_tabular(key, mpi_average(val))
+
+        if rank == 0:
+            logger.dump_tabular()
+
+            if dump_buffer:
+                policy.dump_buffer(epoch)
+
+        # save the policy if it's better than the previous ones
+        success_rate = mpi_average(evaluator.current_success_rate())
+        if rank == 0 and success_rate >= best_success_rate and save_policies:
+            best_success_rate = success_rate
+            logger.info('New best success rate: {}. Saving policy to {} ...'.format(best_success_rate, best_policy_path))
+            evaluator.save_policy(best_policy_path)
+            evaluator.save_policy(latest_policy_path)
+        if rank == 0 and policy_save_interval > 0 and epoch % policy_save_interval == 0 and save_policies:
+            policy_path = periodic_policy_path.format(epoch)
+            logger.info('Saving periodic policy to {} ...'.format(policy_path))
+            evaluator.save_policy(policy_path)
+
+        # make sure that different threads have different seeds
+        local_uniform = np.random.uniform(size=(1,))
+        root_uniform = local_uniform.copy()
+        MPI.COMM_WORLD.Bcast(root_uniform, root=0)
+        if rank != 0:
+            assert local_uniform[0] != root_uniform[0]
+
+
+def launch(env, num_env,
+    env_name, logdir, n_epochs, num_cpu, seed, replay_strategy, policy_save_interval, clip_return,
+    temperature, prioritization, binding, version, dump_buffer, n_cycles, rank_method,
+    w_potential, w_linear, w_rotational, clip_energy, override_params={}, save_policies=False):
+
+    # Fork for multi-CPU MPI implementation.
+    if num_cpu > 1:
+        #whoami = mpi_fork(num_cpu, binding)
+        whoami = mpi_fork(num_cpu)
+        if whoami == 'parent':
+            sys.exit(0)
+        import baselines.common.tf_util as U
+        U.single_threaded_session().__enter__()
+    rank = MPI.COMM_WORLD.Get_rank()
+
+    # Configure logging
+    if rank == 0:
+        logdir = os.path.join(logdir, f"{env_name}_{seed}")
+        if logdir or logger.get_dir() is None:
+            logger.configure(dir=logdir)
+    else:
+        logger.configure()
+    logdir = logger.get_dir()
+    assert logdir is not None
+    os.makedirs(logdir, exist_ok=True)
+    """
+    if logging: 
+        logdir = 'logs/'+str(env_name)+'-temperature'+str(temperature)+\
+                 '-prioritization'+str(prioritization)+'-replay_strategy'+str(replay_strategy)+\
+                 '-n_epochs'+str(n_epochs)+'-num_cpu'+str(num_cpu)+'-seed'+str(seed)+\
+                 '-n_cycles'+str(n_cycles)+'-rank_method'+str(rank_method)+\
+                 '-w_potential'+str(w_potential)+'-w_linear'+str(w_linear)+'-w_rotational'+str(w_rotational)+\
+                 '-clip_energy'+str(clip_energy)+\
+                 '-version'+str(version)
+    else:
+        logdir = osp.join(tempfile.gettempdir(),
+            datetime.datetime.now().strftime("openai-%Y-%m-%d-%H-%M-%S-%f"))
+
+    if rank == 0:
+        if logdir or logger.get_dir() is None:
+            logger.configure(dir=logdir)
+    else:
+        logger.configure()
+    logdir = logger.get_dir()
+    assert logdir is not None
+    os.makedirs(logdir, exist_ok=True)
+    """
+
+    # Seed everything.
+    rank_seed = seed + 1000000 * rank
+    set_global_seeds(rank_seed)
+
+    # Prepare params.
+    params = config.DEFAULT_PARAMS
+    params['env_name'] = env_name
+    params['replay_strategy'] = replay_strategy
+    params['temperature'] = temperature
+    params['prioritization'] = prioritization
+    params['binding'] = binding
+    params['max_timesteps'] = n_epochs * params['n_cycles'] *  params['n_batches'] * num_cpu
+    params['version'] = version
+    params['dump_buffer'] = dump_buffer
+    params['n_cycles'] = n_cycles
+    params['rank_method'] = rank_method
+    params['w_potential'] = w_potential
+    params['w_linear'] = w_linear
+    params['w_rotational'] = w_rotational
+    params['clip_energy'] = clip_energy
+    params['n_epochs'] = n_epochs
+    params['num_cpu'] = num_cpu
+
+    if params['dump_buffer']:
+        params['alpha'] =0
+
+    if env_name.startswith('Point2D'):
+        params.update(config.DEFAULT_ENV_PARAMS['Point2D'])
+    if env_name.startswith('PointMass'):
+        params.update(config.DEFAULT_ENV_PARAMS['PointMass'])
+    elif env_name.startswith('FetchReach'):
+        params.update(config.DEFAULT_ENV_PARAMS['FetchReach'])
+    elif env_name.startswith('Fetch'):
+        params.update(config.DEFAULT_ENV_PARAMS['Fetch'])
+    elif env_name.startswith('SawyerReach'):
+        params.update(config.DEFAULT_ENV_PARAMS['SawyerReach'])
+    elif env_name.startswith('Sawyer'):
+        params.update(config.DEFAULT_ENV_PARAMS['Sawyer'])
+    elif env_name.startswith('Hand'):
+        params.update(config.DEFAULT_ENV_PARAMS['Hand'])
+    if env_name in config.DEFAULT_ENV_PARAMS:
+        params.update(config.DEFAULT_ENV_PARAMS[env_name])  # merge env-specific parameters in
+    params.update(**override_params)  # makes it possible to override any parameter
+    params = config.prepare_params(params)
+    params['rollout_batch_size'] = num_env
+
+    with open(os.path.join(logger.get_dir(), 'params.json'), 'w') as f:
+        dump_params = copy.deepcopy(params)
+        for key, value in params.items():
+            dump_params[key] = str(value)
+        json.dump(dump_params, f)
+    config.log_params(params, logger=logger)
+
+    random_init = params['random_init']
+    dims = config.configure_dims(params)
+    policy = config.configure_ddpg(dims=dims, params=params, clip_return=clip_return)
+
+    rollout_params = {
+        'exploit': False,
+        'use_target_net': False,
+        'use_demo_states': True,
+        'compute_Q': False,
+        'T': params['T'],
+    }
+
+    eval_params = {
+        'exploit': True,
+        'use_target_net': params['test_with_polyak'],
+        'use_demo_states': False,
+        'compute_Q': True,
+        'T': params['T'],
+    }
+
+    for name in ['T', 'rollout_batch_size', 'gamma', 'noise_eps', 'random_eps']:
+        rollout_params[name] = params[name]
+        eval_params[name] = params[name]
+
+    eval_env =  env
+    rollout_worker = RolloutWorker(env, policy, dims, logger, monitor=True, **rollout_params)
+    evaluator = RolloutWorker(eval_env, policy, dims, logger, **eval_params)
+
+    train(
+        logdir=logdir, policy=policy, rollout_worker=rollout_worker,
+        evaluator=evaluator, n_epochs=n_epochs, n_test_rollouts=params['n_test_rollouts'],
+        n_cycles=params['n_cycles'], n_batches=params['n_batches'],
+        policy_save_interval=policy_save_interval, save_policies=save_policies,
+        num_cpu=num_cpu, dump_buffer=dump_buffer, w_potential=params['w_potential'], 
+        w_linear=params['w_linear'], w_rotational=params['w_rotational'], rank_method=rank_method,
+        clip_energy=clip_energy, random_init=random_init)
+
+
+@click.command()
+@click.option('--env_name', type=str, default='FetchReach-v1', help='the name of the OpenAI Gym environment that you want to train on. We tested EBP on four challenging robotic manipulation tasks')
+@click.option('--logdir', type=str, default='~/results/herbp', help='the path to where logs and policy pickles should go. If not specified, creates a folder in /tmp/')
+@click.option('--n_epochs', type=int, default=50, help='the number of training epochs to run')
+@click.option('--num_cpu', type=int, default=1, help='the number of CPU cores to use (using MPI)')
+@click.option('--num_env', type=int, default=1, help='Number of environment copies being run')
+@click.option('--seed', type=int, default=0, help='the random seed used to seed both the environment and the training code')
+@click.option('--policy_save_interval', type=int, default=5, help='the interval with which policy pickles are saved. If set to 0, only the best and latest policy will be pickled.')
+@click.option('--replay_strategy', type=click.Choice(['future', 'final', 'none']), default='future', help='the HER replay strategy to be used. "future" uses HER, "none" disables HER.')
+@click.option('--clip_return', type=int, default=1, help='whether or not returns should be clipped')
+@click.option('--temperature', type=float, default=1.0, help='temperature value for Enery-Based Prioritization (EBP)')
+@click.option('--prioritization', type=click.Choice(['none', 'energy', 'tderror']), default='energy', help='the prioritization strategy to be used. "energy" uses EBP;\
+                                                                                                             "none" is vanilla HER; tderror is Prioritized Experience Replay.')
+@click.option('--binding', type=click.Choice(['none', 'core']), default='core', help='configure mpi using bind-to none or core.')
+#@click.option('--logging', type=bool, default=False, help='whether or not logging')
+@click.option('--version', type=int, default=0, help='version')
+@click.option('--dump_buffer', type=bool, default=False, help='dump buffer contains achieved goals, energy, tderrors for analysis')
+@click.option('--n_cycles', type=int, default=25, help='n_cycles')
+@click.option('--rank_method', type=click.Choice(['none', 'min', 'dense', 'average']), default='none', help='energy ranking method')
+@click.option('--w_potential', type=float, default=1.0, help='w_potential')
+@click.option('--w_linear', type=float, default=1.0, help='w_linear')
+@click.option('--w_rotational', type=float, default=1.0, help='w_rotational')
+@click.option('--clip_energy', type=float, default=999, help='clip_energy')
+
+def main(**kwargs):
+    env = build_env(kwargs, _game_envs)
+    kwargs.update({"env": env})
+    launch(**kwargs)
+
+if __name__ == '__main__':
+    main()
