46833d9c99bf44eac34ccebcf8327f5731ba089b
ztjiaweixu <ztjiaweixu@tencent.com>
2021-12-27 20:03:25 +0800

    update cher config

--- baselines/cher/experiment/config.py
+++ baselines/cher/experiment/config.py
@@ -44,7 +44,7 @@ DEFAULT_PARAMS = {
     'Q_lr': 0.001,  # critic learning rate
     'pi_lr': 0.001,  # actor learning rate
     'buffer_size': int(1E6),  # for experience replay
-    'polyak': 0.9,  # polyak averaging coefficient
+    'polyak': 0.95,  # polyak averaging coefficient
     'action_l2': 1.0,  # quadratic penalty on actions (before rescaling by max_u)
     'clip_obs': 200.,
     'scope': 'ddpg',  # can be tweaked for testing
@@ -53,8 +53,8 @@ DEFAULT_PARAMS = {
     'n_cycles': 50,  # per epoch
     'rollout_batch_size': 1,  # per mpi thread
     'n_batches': 40,  # training batches per cycle
-    'batch_size': 64,  # per mpi thread, measured in transitions and reduced to even multiple of chunk_length.
-    'n_test_rollouts': 100,  # number of test rollouts per epoch, each consists of rollout_batch_size rollouts
+    'batch_size': 256,  # per mpi thread, measured in transitions and reduced to even multiple of chunk_length.
+    'n_test_rollouts': 10,  # number of test rollouts per epoch, each consists of rollout_batch_size rollouts
     'test_with_polyak': False,  # run test episodes with the target network
     # exploration
     'random_eps': 0.3,  # percentage of time a random action is taken
@@ -63,7 +63,7 @@ DEFAULT_PARAMS = {
     'replay_strategy': 'future',  # supported modes: future, none
     'replay_k': 4,  # number of additional goals used for replay, only used if off_policy_data=future
     # normalization
-    'norm_eps': 1e-4,  # epsilon used for observation normalization
+    'norm_eps': 0.01,  # epsilon used for observation normalization
     'norm_clip': 5,  # normalized observations are cropped to this values
     # random init episode
     'random_init': 20,
